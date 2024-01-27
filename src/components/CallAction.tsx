e5b31f1e37278dcc87ff8444ea4d4869ecb9ae5c
Ivan <vanyok.korobka@ukr.net>
2024-01-28 01:19:45 +0200

    prepare adaptive for CallAction section

--- src/components/CallAction.tsx
+++ src/components/CallAction.tsx
@@ -1,9 +1,10 @@
 
 const CallAction = () => {
   return (
-    <section className="bg-[#1F0B0B] mt-[150px]">
+    <section className="bg-[#1F0B0B] mt-[80px] sm:mt-[100px] bmd:mt-[150px]">
       <div className="max-w-[1240px] w-full h-full m-auto px-[16px] sm:[25px] md:px-[50px] xl:px-0">
-        <div className="flex justify-center items-center gap-[200px] w-full py-[70px]">
+        <div className="flex flex-col bmd:flex-row bmd:justify-center items-center gap-[40px] sm:gap-[70px] 
+        bmd:gap-[125px] xl:gap-[200px] w-full py-[40px] sm:py-[70px]">
           <div className="flex flex-col max-w-[400px] w-full">
             <p className="text-[#eee] font-raleway text-[20px] font-black leading-[150%] max-w-[327px] w-full">
               Fill out the form, and we will get in touch with you shortly to provide detailed consultation
@@ -13,17 +14,17 @@ const CallAction = () => {
               type="text"
               placeholder="Name"
               className="mt-[30px] pb-[10px] border-b border-b-[#eee] bg-transparent placeholder:text-[#B7B7B7] 
-              text-[#EEE] font-outfit text-base font-normal outline-none"
+              text-[#EEE] font-outfit text-sm sm:text-base font-normal outline-none"
             />
 
             <input
               type="tel"
               placeholder="Phone number"
               className="mt-[30px] pb-[10px] border-b border-b-[#eee] bg-transparent placeholder:text-[#B7B7B7] 
-              text-[#EEE] font-outfit text-base font-normal outline-none"
+              text-[#EEE] font-outfit text-sm sm:text-base font-normal outline-none"
             />
 
-            <div className="flex items-center gap-[10px] mt-[10px]">
+            <div className="flex items-center gap-[10px] mt-[13px]">
               <input type="checkbox" id="terms" className="rounded-full checkbox border-[#EEEEEE] 
               checked:border-[#FFBE42] [--chkbg:theme(mytheme.secondary)] checked:bg-[url('/icons/check-alt.svg')] 
               bg-cover [--chkfg:theme(mytheme.success)] w-[24px] h-[24px]" />
@@ -35,15 +36,15 @@ const CallAction = () => {
               </label>
             </div>
 
-            <button className="mt-[32px] py-5 text-center text-[#1F0B0B] font-outfit text-[20px] font-semibold 
-            leading-[100%] bg-[#FFBE42] rounded-[20px] border-2 border-solid border-transparent 
+            <button className="mt-[32px] py-5 text-center text-[#1F0B0B] font-outfit sm:text-[20px] font-normal 
+            sm:font-semibold leading-[100%] bg-[#FFBE42] rounded-[20px] border-2 border-solid border-transparent 
             hover:border-[#FFBE42] hover:bg-transparent hover:text-[#FFBE42] transition-all ease-in-out">
               Order a call
             </button>
           </div>
 
           <h2 className="flex flex-col gap-5 text-[#EEE]">
-            <div className="font-outfit leading-[100%] text-[96px]">
+            <div className="font-outfit leading-[100%] text-[96px] flex flex-wrap">
               <span className="font-black">20%</span>
               <span className="text-[64px] font-semibold">Discount</span>
             </div>
