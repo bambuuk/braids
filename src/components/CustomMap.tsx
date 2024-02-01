f4b57d44d0f02e2b1c07b2596f7a644ee2a1ee7c
Ivan <vanyok.korobka@ukr.net>
2024-02-01 20:35:44 +0200

    prepare map from leaflet.js in map component

---
+++ src/components/CustomMap.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import React from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import MarkerIcon from '../../node_modules/leaflet/dist/images/marker-icon.png';
+import MarkerShadow from '../../node_modules/leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/leaflet.css';
+import Image from 'next/image';
+
+const CustomMap = () => {
+  return (
+    <MapContainer center={[50.51179, 30.58977]} zoom={14} scrollWheelZoom={false} className='w-full h-full'>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker
+        position={[50.51179, 30.58977]}
+        icon={
+          new L.Icon({
+            iconUrl: MarkerIcon.src,
+            iconRetinaUrl: MarkerIcon.src,
+            iconSize: [25, 41],
+            iconAnchor: [12.5, 41],
+            popupAnchor: [0, -41],
+            shadowUrl: MarkerShadow.src,
+            shadowSize: [41, 41],
+          })
+        }
+      >
+        <Popup className='p-[20px] sm:p-[30px] rounded-[10px] bg-[#1F0B0B] opacity-80
+        shadow-little flex flex-col'>
+          <div className="flex items-center gap-[10px] font-outfit text-[16px] font-semibold leading-[100%] w-full">
+            <Image
+              src='/icons/location.png'
+              width={24}
+              height={24}
+              alt=""
+              className='w-6 h-6 object-cover'
+            />
+            <span className='text-[#EEEEEE] text-[20px] font-semibold leading-[100%]'>Kyiv, Myru Street, 25-a</span>
+          </div>
+          <div className="flex items-center gap-[10px] font-outfit">
+            <Image
+              src='/icons/phone.png'
+              width={24}
+              height={24}
+              alt=""
+              className='w-6 h-6 object-cover second'
+            />
+            <a href="tel:+380631231212" className='text-[#EEEEEE] text-[16px] font-semibold leading-[100%]'>+38(063)-123-12-12</a>
+          </div>
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
+
+export default CustomMap
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,59 +1,15 @@
 'use client';
-import useMediaQuery from '@/hooks/useMediaQuery';
-import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const DynamicMap = dynamic(() => import('./CustomMap'), {
+  loading: () => <p className='text-black flex justify-center items-center h-full'>Loading...</p>,
+  ssr: false
+})
 
 const Map = () => {
-  const { isMobile, isSmallTablet, isTablet, isDesktop } = useMediaQuery();
-  const [imgSrc, setImgSrc] = useState("/map-tablet.png");
-
-  useEffect(() => {
-    setImgSrc(
-      isDesktop
-        ? "/map.png"
-        : isSmallTablet
-          ? "/map-tablet.png"
-          : isMobile
-            ? "/map-mobile.png"
-            : "/map.png"
-    );
-  }, [isMobile, isSmallTablet, isTablet, isDesktop]);
-
   return (
     <div id="map" className='w-full h-[447px] mt-[80px] md:mt-[100px] lg:mt-[150px] relative'>
-      <Image
-        src={imgSrc}
-        width={1440}
-        height={447}
-        alt="Map"
-        className='w-full h-full object-cover'
-      />
-      <div
-        className='absolute z-10 p-[20px] sm:p-[30px] bottom-[40%] sm:bottom-[170px] left-[10px] 
-        sm:left-[15%] rounded-[20px] bg-[#1F0B0B] opacity-80
-        shadow-little flex flex-col gap-5 font-outfit text-[20px] font-semibold leading-[100%]'
-      >
-        <div className="flex items-center gap-[10px]">
-          <Image
-            src='/icons/location.png'
-            width={24}
-            height={24}
-            alt=""
-            className='w-6 h-6 object-cover'
-          />
-          <span>Kyiv, Myru Street, 25-a</span>
-        </div>
-        <div className="flex items-center gap-[10px]">
-          <Image
-            src='/icons/phone.png'
-            width={24}
-            height={24}
-            alt=""
-            className='w-6 h-6 object-cover'
-          />
-          <a href="tel:+380631231212">+38(063)-123-12-12</a>
-        </div>
-      </div>
+      <DynamicMap />
     </div>
   )
 }
